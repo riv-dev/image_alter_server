1fbeb4d1bdd2d74feda4a85e68e8b43765a9fdb7
Ken Le <ken@ryukyu-i.co.jp>
2017-04-07 14:38:11 +0700

    Initial commit

---
+++ image_alter_server.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var diffMatchPatch = require('./diff_match_patch.js');
+var fs = require("fs");
+
+
+
+var app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+var file_path;
+
+
+// Checks Sync
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage: ");
+   console.log("  host: " + req.query.host);
+   console.log("  relative: " + req.query.relative_uri);
+   console.log("  rootpath: "  + req.query.rootpath);
+
+   //find the file
+    var rootpath = decodeURIComponent(req.query.rootpath);
+    var relative_uri = decodeURIComponent(req.query.relative_uri);
+    file_path = rootpath + "/" + relative_uri;
+    var regex = /.html$/i;
+
+    if(!regex.test()) {
+        file_path = file_path + "/index.html";
+    }
+
+    console.log("  filepath: " + file_path);
+
+   if(req.query.rootpath && req.query.rootpath.length > 0) {
+        var rootpath = decodeURIComponent(req.query.rootpath);
+        if (fs.existsSync(file_path)) {
+            res.json({ status: "online", synced: "success", rootpath: file_path});
+        } else {
+            res.json({ status: "online", synced: "error", rootpath: "Does not exist"});
+        }
+   } else {
+       res.json({status: "online", synced: "error", rootpath: "Define rootpath"});
+   }
+});
+
+// This server only listens to POST requests at "/" url
+app.post('/', urlencodedParser, function (req, res) {
+   console.log("Got a POST request for the homepage: ");
+   console.log("  host: " + req.body.host);
+   console.log("  relative: " + req.body.relative_uri);
+   console.log("  rootpath: "  + req.body.rootpath);
+
+   //find the file
+    var rootpath = decodeURIComponent(req.body.rootpath);
+    var relative_uri = decodeURIComponent(req.body.relative_uri);
+    file_path = rootpath + "/" + relative_uri;
+    var regex = /.html$/i;
+
+    if(!regex.test()) {
+        file_path = file_path + "/index.html";
+    }
+
+    console.log("  filepath: " + file_path);
+
+    var patchedHTML;
+    var diffMatchPatchObj = diffMatchPatch.diff_match_patch();
+
+    // Asynchronous read
+    if (fs.existsSync(file_path)) { 
+        fs.readFile(file_path, function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            //console.log("Asynchronous read: " + data.toString());
+            patchedHTML = diffMatchPatchObj.patch_apply(JSON.parse(req.body.patch), data.toString())[0];
+
+            fs.writeFile(file_path, patchedHTML, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                console.log("Changed file: " + file_path);
+            });
+
+            console.log("Success");
+            res.json({status: "success", filepath: file_path, patch: req.body.patch});
+        });
+    } else {
+        console.log("Error");
+        res.json({status: "error", message: "File does not exist"});
+    }
+
+});
+
+var server = app.listen(9255, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
